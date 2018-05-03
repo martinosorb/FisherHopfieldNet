6945ede24dbd80ebb44607263804b656aef3195a
martinosorb <martino.sorbaro@ed.ac.uk>
2018-05-03 17:34:59 +0100

    big cleanup

--- code/run_trials.py
+++ code/run_trials.py
@@ -21,17 +21,28 @@ def dice_coefficient(p1, p2):
     n = np.sum(p1)+np.sum(p2)
     return p/n
 
+
+def evaluate_stability(network, patterns):
+    overall_error = 0
+    for p in patterns:
+        network.present_pattern(p)
+        network.step(eval_epochs)
+        output = netFisher.s
+        overall_error += dice_coefficient(p, output)
+    return overall_error/len(patterns)
+
+
 ETA = 0.001  # learning rate
 NTRAIN = 2000  # number of epochs
 NUM_PATTERNS = 1000  # number of patterns created
 SPARSITY = 0.1  # number of zeros: e.g. SPARSITY = 0.1 means 10% ones and 90% zeros
 IMAGE_SIZE = 10  # the size of our created pattern will be (IMAGE_SIZE x IMAGE_SIZE)
 eval_f = 1  # evaluation frequency (every eval_f-th iteration)
-TRIALS = 300  # number of trials over which the results will be averaged in order to get smooth results
+TRIALS = 3  # number of trials over which the results will be averaged
 less_changed_weight_value = 0.00
 # the learning rate of weights which are considered important have a
 # learning rate of ETA * less_changed_weight_value
-stored_patterns = 5  # number of patterns that are stored in the network before learning the new pattern
+stored_patterns = 5  # number of patterns that are stored in the network before learning
 number_of_changed_values = 4750
 # the number of weigths that are changed is 2*number_of_changed_values
 # (The factor of 2 is because of the symmetry of the weight matrix)
@@ -43,16 +54,20 @@ RUN_FIH = False  # the experiment shows it's the same...
 RUN_FLT = True
 
 # Define variables
-complete_error_new_pattern = -np.ones(shape=(TRIALS, NTRAIN))
-complete_error_mean = -np.ones(shape=(TRIALS, NTRAIN))
-complete_error_new_patternFL = -np.ones(shape=(TRIALS, NTRAIN))
-complete_error_meanFL = -np.ones(shape=(TRIALS, NTRAIN))
-complete_error_new_patternFLT = -np.ones(shape=(TRIALS, NTRAIN))
-complete_error_meanFLT = -np.ones(shape=(TRIALS, NTRAIN))
-complete_error_new_patternFI = -np.ones(shape=(TRIALS, NTRAIN))
-complete_error_meanFI = -np.ones(shape=(TRIALS, NTRAIN))
-complete_error_new_patternFIH = -np.ones(shape=(TRIALS, NTRAIN))
-complete_error_meanFIH = -np.ones(shape=(TRIALS, NTRAIN))
+Errors = {}
+Errors['trad_80_N'] = -np.ones(shape=(TRIALS, NTRAIN))
+Errors['trad_80_O'] = -np.ones(shape=(TRIALS, NTRAIN))
+if RUN_FL:
+    Errors['FL_N'] = -np.ones(shape=(TRIALS, NTRAIN))
+    Errors['FL_O'] = -np.ones(shape=(TRIALS, NTRAIN))
+if RUN_FLT:
+    Errors['FLT_N'] = -np.ones(shape=(TRIALS, NTRAIN))
+    Errors['FLT_O'] = -np.ones(shape=(TRIALS, NTRAIN))
+Errors['FI_N'] = -np.ones(shape=(TRIALS, NTRAIN))
+Errors['FI_O'] = -np.ones(shape=(TRIALS, NTRAIN))
+if RUN_FIH:
+    Errors['FIH_N'] = -np.ones(shape=(TRIALS, NTRAIN))
+    Errors['FIH_O'] = -np.ones(shape=(TRIALS, NTRAIN))
 
 
 print('**Started Learning**')
@@ -87,20 +102,10 @@ for trial in range(0, TRIALS):
         netFisher.set_weights(wF)
 
         # checking stability of patterns after eval_epochs iterations
-        overall_error = 0
-        for i in range(int(stored_patterns)):
-            netFisher.present_pattern(original_patterns[:, i])
-            netFisher.step(eval_epochs)
-            output = netFisher.s
-            overall_error += dice_coefficient(original_patterns[:, i], output)
-
-        netFisher.present_pattern(original_patterns[:, stored_patterns+1])
-        netFisher.step(eval_epochs)
-        output = netFisher.s
-        error = dice_coefficient(original_patterns[:, stored_patterns+1], output)
-
-        complete_error_mean[trial, epoch] = overall_error/stored_patterns
-        complete_error_new_pattern[trial, epoch] = error
+        Errors['trad_80_O'][trial, epoch] = evaluate_stability(  # old patterns
+            netFisher, original_patterns.T[:stored_patterns])
+        Errors['trad_80_N'][trial, epoch] = evaluate_stability(  # new pattern
+            netFisher, [original_patterns.T[stored_patterns+1]])
 
     wH_final = netFisher.w
 
@@ -138,20 +143,10 @@ for trial in range(0, TRIALS):
             netFisher.set_weights(wF)
 
             # checking stability of patterns after eval_epochs iterations
-            overall_error = 0
-            for i in range(int(stored_patterns)):
-                netFisher.present_pattern(original_patterns[:, i])
-                netFisher.step(eval_epochs)
-                output = netFisher.s
-                overall_error += dice_coefficient(original_patterns[:, i], output)
-
-            netFisher.present_pattern(original_patterns[:, stored_patterns+1])
-            netFisher.step(eval_epochs)
-            output = netFisher.s
-            error = dice_coefficient(original_patterns[:, stored_patterns+1], output)
-
-            complete_error_meanFL[trial, epoch] = overall_error/stored_patterns
-            complete_error_new_patternFL[trial, epoch] = error
+            Errors['FL_O'][trial, epoch] = evaluate_stability(  # old patterns
+                netFisher, original_patterns.T[:stored_patterns])
+            Errors['FL_N'][trial, epoch] = evaluate_stability(  # new pattern
+                netFisher, [original_patterns.T[stored_patterns+1]])
 
         wFL_final = netFisher.w
 
@@ -178,20 +173,10 @@ for trial in range(0, TRIALS):
             netFisher.set_weights(wF)
 
             # checking stability of patterns after eval_epochs iterations
-            overall_error = 0
-            for i in range(int(stored_patterns)):
-                netFisher.present_pattern(original_patterns[:, i])
-                netFisher.step(eval_epochs)
-                output = netFisher.s
-                overall_error += dice_coefficient(original_patterns[:, i], output)
-
-            netFisher.present_pattern(original_patterns[:, stored_patterns+1])
-            netFisher.step(eval_epochs)
-            output = netFisher.s
-            error = dice_coefficient(original_patterns[:, stored_patterns+1], output)
-
-            complete_error_meanFLT[trial, epoch] = overall_error/stored_patterns
-            complete_error_new_patternFLT[trial, epoch] = error
+            Errors['FLT_O'][trial, epoch] = evaluate_stability(  # old patterns
+                netFisher, original_patterns.T[:stored_patterns])
+            Errors['FLT_N'][trial, epoch] = evaluate_stability(  # new pattern
+                netFisher, [original_patterns.T[stored_patterns+1]])
 
         wFLT_final = netFisher.w
 
@@ -225,20 +210,10 @@ for trial in range(0, TRIALS):
         netFisher.set_weights(wF)
 
         # checking stability of patterns after eval_epochs iterations
-        overall_error = 0
-        for i in range(int(stored_patterns)):
-            netFisher.present_pattern(original_patterns[:, i])
-            netFisher.step(eval_epochs)
-            output = netFisher.s
-            overall_error += dice_coefficient(original_patterns[:, i], output)
-
-        netFisher.present_pattern(original_patterns[:, stored_patterns+1])
-        netFisher.step(eval_epochs)
-        output = netFisher.s
-        error = dice_coefficient(original_patterns[:, stored_patterns+1], output)
-
-        complete_error_meanFI[trial, epoch] = overall_error/stored_patterns
-        complete_error_new_patternFI[trial, epoch] = error
+        Errors['FI_O'][trial, epoch] = evaluate_stability(  # old patterns
+            netFisher, original_patterns.T[:stored_patterns])
+        Errors['FI_N'][trial, epoch] = evaluate_stability(  # new pattern
+            netFisher, [original_patterns.T[stored_patterns+1]])
 
 
 # ======== FIH ========= #
@@ -271,46 +246,15 @@ for trial in range(0, TRIALS):
             netFisher.set_weights(wF)
 
             # checking stability of patterns after eval_epochs iterations
-            overall_error = 0
-            for i in range(int(stored_patterns)):
-                netFisher.present_pattern(original_patterns[:, i])
-                netFisher.step(eval_epochs)
-                output = netFisher.s
-                overall_error += dice_coefficient(original_patterns[:, i], output)
-
-            netFisher.present_pattern(original_patterns[:, stored_patterns+1])
-            netFisher.step(eval_epochs)
-            output = netFisher.s
-            error = dice_coefficient(original_patterns[:, stored_patterns+1], output)
-
-            complete_error_meanFIH[trial, epoch] = overall_error/stored_patterns
-            complete_error_new_patternFIH[trial, epoch] = error
+            Errors['FIH_O'][trial, epoch] = evaluate_stability(  # old patterns
+                netFisher, original_patterns.T[:stored_patterns])
+            Errors['FIH_N'][trial, epoch] = evaluate_stability(  # new pattern
+                netFisher, [original_patterns.T[stored_patterns+1]])
 
         wFIH_final = netFisher.w
 
-savedict = {
-    'trad_80_O': complete_error_mean,
-    'trad_80_N': complete_error_new_pattern,
-    'FI_O': complete_error_meanFI,
-    'FI_N': complete_error_new_patternFI}
 
-keys = ['trad_80_O', 'trad_80_N']
-
-if RUN_FL:
-    savedict['FL_O'] = complete_error_meanFL
-    savedict['FL_N'] = complete_error_new_patternFL
-    keys += ['FL_O', 'FL_N']
-if RUN_FLT:
-    savedict['FLT_O'] = complete_error_meanFLT
-    savedict['FLT_N'] = complete_error_new_patternFLT
-    keys += ['FLT_O', 'FLT_N']
-keys += ['FI_O', 'FI_N']
-if RUN_FIH:
-    savedict['FIH_O'] = complete_error_meanFIH
-    savedict['FIH_N'] = complete_error_new_patternFIH
-    keys += ['FIH_O', 'FIH_N']
-
-filename = "../Complete_errors_stored{}_size{}.npz".format(stored_patterns, IMAGE_SIZE)
-np.savez(filename, **savedict)
+filename = "../prova_Complete_errors_stored{}_size{}.npz".format(stored_patterns, IMAGE_SIZE)
+np.savez(filename, **Errors)
 
 print('**Finished**')
